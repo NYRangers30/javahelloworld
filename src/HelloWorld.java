004a516e341bd9cc7836b663353427bcd87d668e
NYRangers30 <nyrangers30@gmail.com>
2017-04-11 15:18:24 +0000

    updated print message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main (String [] args) {
-        System.out.println("hello world from java");
+        System.out.println("hello automated world from java");
     }
 }
